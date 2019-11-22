b29e1d9378d69a9760786a371c13922dfbb974a5
Denis <denislavrovk@gmail.com>
2019-11-22 22:22:31 +0300

    changed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 module.exports = (app) => {
 	// Все GET запросы тут
 	app.get('/', require('./main'));
-	app.get('/api/user/:id?', require('./user').get);
 	app.get('/api/user/search', require('./search').get);
 	// Все POST запросы тут
 	app.post('/api/user/add', require('./user').add);
