25b8d67da62471c2aed246b5b5bba9b0a16f8e37
LilKoi <Li_koi@mail.ru>
2019-11-23 01:47:25 +0400

    fix id_2

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 module.exports = (app) => {
 	// Все GET запросы тут
 	app.get('/', require('./main'));
-<<<<<<< HEAD
-	app.get('/api/user/:id?/', require('./user').get);
-=======
->>>>>>> b29e1d9378d69a9760786a371c13922dfbb974a5
+	app.get('/api/user/:id/', require('./user').get);
 	app.get('/api/user/search', require('./search').get);
 	// Все POST запросы тут
 	app.post('/api/user/', require('./user').add);
--- routes/search.js
+++ routes/search.js
@@ -2,13 +2,6 @@ const { User } = require('../models/user');
 
 
 exports.get = async (req, res) => {
-<<<<<<< HEAD
-    
     const search = req.query.name;
     res.send({search});
-=======
-const search = req.query.name;
-console.log(req.query);
-res.send({search});
->>>>>>> b29e1d9378d69a9760786a371c13922dfbb974a5
 }
